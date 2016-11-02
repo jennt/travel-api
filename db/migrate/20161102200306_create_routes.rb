eb05483b8e4b7fbd76a21a75ec8fc2a5208b116a
Jenn Tustin <jenn.tustin@gmail.com>
2016-11-02 16:04:16 -0400

    Add route model

---
+++ db/migrate/20161102200306_create_routes.rb
@@ -0,0 +1,14 @@
+class CreateRoutes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :routes do |t|
+      t.string :route
+      t.string :name
+      t.string :street
+      t.string :city
+      t.string :state
+      t.integer :zip
+
+      t.timestamps
+    end
+  end
+end
