002515a375d62f3a51f2cdc56bf515206b67b092
Jenn Tustin <jenn.tustin@gmail.com>
2016-11-06 16:41:20 -0500

    Refactor routes.rb

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -3,17 +3,13 @@ class RoutesController < ApplicationController
   attr_accessor :route, :crew, :name, :street, :city, :state, :zip
 
   def index
-    @routes = Route.all
-    @routes.each { |r| r.task_list = Task.where(route_id: r.id) }
+    add_each_task_list
 
     render :json => @routes
-    # respond_to :json
   end
 
   def show
-    @routes = Route.all
-    @routes.each { |r| r.task_list = Task.where(route_id: r.id) }
-
+    add_each_task_list
     @routes = @routes.group_by {|x| x['route']}
 
     render :json => @routes
@@ -21,7 +17,9 @@ class RoutesController < ApplicationController
   end
 
   def create
-    @route = Route.new route_params#(route: params['route'], crew: params['crew'], name: params['name'], street: params['street'], city: params['city'], state: params['state'], zip: params['zip'], task_list: params['task_list'])
+    @route = Route.new route_params
+
+    add_task_list
 
     if @route.save
       render :json => @route
@@ -33,9 +31,10 @@ class RoutesController < ApplicationController
 
   def update
     @route = Route.find(params[:id])
-    @route.task_list = Task.where(route_id: @route.id)
 
-    if @route.update route_params#(route: params['route'], crew: params['crew'], name: params['name'], street: params['street'], city: params['city'], state: params['state'], zip: params['zip'], task_list: params['task_list'])
+    add_task_list
+
+    if @route.update route_params
       render :json => @route
     else
       render_error @route.errors.full_messages
@@ -44,13 +43,22 @@ class RoutesController < ApplicationController
 
   def destroy
     @location = Route.find(params[:id])
+
     if @location.destroy
-      @routes = Route.all
-      @routes.each { |r| r.task_list = Task.where(route_id: r.id) }
+      add_each_task_list
       render :json => @routes
     else
       render_error @location.errors.full_messages
     end
+  end
+
+  def add_each_task_list
+    @routes = Route.all
+    @routes.each { |r| r.task_list = Task.where(route_id: r.id) }
+  end
+
+  def add_task_list
+    @route.task_list = Task.where(route_id: @route.id)
   end
 
   private
